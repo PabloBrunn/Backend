1ef3a1ec12ef7f3b3f0475620babac4b2342ee4e
Pablo Brun <pablobrunn@outlook.com>
2023-08-01 23:28:58 -0300

    primera pre-entrega

---
+++ src/productManager.js
@@ -0,0 +1,90 @@
+import fs from 'fs'
+
+class ProductManager {
+    constructor(path) {
+        this.path = path
+    }
+
+    async getProducts() {
+        try {
+            if (fs.existsSync(this.path)) {
+                const infoArchivo = await fs.promises.readFile(this.path, 'utf-8')
+                return JSON.parse(infoArchivo)
+            } else {
+                return []
+            }
+        } catch (error) {
+            return error
+        }
+    }
+
+
+    async addProduct(obj) {
+        try {
+            const arrayProd = await this.getProducts()
+            let id
+            if (!arrayProd.length) {
+                id = 1
+            } else {
+                id = arrayProd[arrayProd.length - 1].id + 1
+            }
+            arrayProd.push({ ...obj, id })
+            await fs.promises.writeFile(this.path, JSON.stringify(arrayProd))
+            return arrayProd
+        } catch (error) {
+            return error
+        }
+    }
+
+
+    async getProductById(id) {
+        try {
+            const arrayProd = await this.getProducts()
+            const producto = arrayProd.find((p) => p.id === id)
+            if (!producto) {
+                return 'Usuario con id no encontrado'
+            }
+            return producto
+        } catch (error) {
+            return error
+        }
+    }
+
+
+
+    async updateProduct(id, obj) {
+        try {
+            const arrayProd = await this.getProducts()
+            const prodEncontrado = arrayProd.findIndex((u) => u.id === id)
+            if (prodEncontrado === -1) {
+                return 'No hay un producto con ese id'
+            }
+            const producto = arrayProd[prodEncontrado]
+            arrayProd[prodEncontrado] = { ...producto, ...obj }
+            await fs.promises.writeFile(this.path, JSON.stringify(arrayProd))
+        } catch (error) {
+            return error
+        }
+    }
+
+
+    async deleteProduct(id) {
+        try {
+            const arrayProd = await this.getProducts()
+            const nuevoArrayProductos = arrayProd.filter((u) => u.id !== id)
+            await fs.promises.writeFile(
+                this.path,
+                JSON.stringify(nuevoArrayProductos)
+            )
+        } catch (error) {
+            return error
+        }
+    }
+
+}
+
+
+
+
+export default ProductManager;
+
---
+++ src/routes/cart.routes.js
@@ -0,0 +1,40 @@
+import { Router } from "express";
+import CarritoManager from "../cartManager.js";
+
+
+const manager = new CarritoManager('./Carrito.json');
+
+const router = Router()
+
+router.get('/id', async (req, res) => {
+    const {id} = req.params
+    try{
+        const carrito = await manager.getOneCart(+id)
+        res.status(200).json({ message: 'Carrito',carrito})
+    } catch(error) {
+        res.status(500).json({error})
+    }
+})
+
+router.post('/',async(req,res)=>{
+    try{
+        const crearCarrito = await manager.crearCarrito()
+        res.status(200).json({ message: 'carrito', carrito:crearCarrito})
+    }catch (error) {
+        res.status(500).json({error})
+    }
+})
+
+
+router.post('/:idCarrito/carritos/:idProd',async(req,res)=>{
+    const {idCarrito,idProd} = req.params
+    try {
+        const addProd = await manager.addProd(+idCarrito,+idProd)
+        res.status(200).json({message:'prod-Carrito',carrito:addProd})
+    } catch (error) {
+        res.status(500).json({error})
+    }
+})
+
+
+export default CarritoManager
